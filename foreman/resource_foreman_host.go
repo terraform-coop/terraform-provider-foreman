b9725041e588f0dbe455964a0f96cf25cef8dfea
Lennart Weller <lennart.weller@hansemerkur.de>
2021-02-22 15:16:03 +0100

    Renable compute_profile_id computation attribute

--- foreman/resource_foreman_host.go
+++ foreman/resource_foreman_host.go
@@ -179,6 +179,7 @@ func resourceForemanHost() *schema.Resource {
 			"compute_profile_id": &schema.Schema{
 				Type:         schema.TypeInt,
 				Optional:     true,
+				Computed:     true,
 				ForceNew:     false,
 				ValidateFunc: validation.IntAtLeast(0),
 			},
