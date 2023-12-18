fbf0650ad3c818185eae74e2dab45ae7d072c0f2
Dominik Pataky <pataky@mindful-security.eu>
2023-12-18 13:02:05 +0100

    job_templates: Remove new utils functions from branch
    They are to be included in an extra branch, see discussion
    https://github.com/terraform-coop/terraform-provider-foreman/pull/138#discussion_r1425588236
    
    Signed-off-by: Dominik Pataky <pataky@mindful-security.eu>

--- foreman/utils/utils.go
+++ foreman/utils/utils.go
@@ -2,61 +2,18 @@ package utils
 
 // Provides util functions for the provider package
 
-import (
-	"path"
-	"runtime"
-	"strings"
-
-	"github.com/HanseMerkur/terraform-provider-utils/log"
-)
-
-// Source: https://www.reddit.com/r/golang/comments/f04v0l/debug_and_current_function_name/
 func TraceFunctionCall() {
-	pc, file, line, ok := runtime.Caller(1)
-	if !ok {
-		log.Fatal("TraceFunctionCall failed")
-	}
-
-	dirName, fileName := path.Split(file)
-
-	parts := strings.Split(runtime.FuncForPC(pc).Name(), ".")
-	pl := len(parts)
-	funcName := parts[pl-1]
-	if parts[pl-2][0] == '(' {
-		funcName = parts[pl-2] + "." + funcName
-	}
-
-	prefix_path := strings.TrimPrefix(dirName, "github.com/terraform-coop/terraform-provider-foreman/")
-
-	log.Tracef("%s (%s%s:%d)", funcName, prefix_path, fileName, line)
+	// Removed in branch feat/job_templates, to be filled in separate branch
 }
 
 // Like `log.Debugf` but also prints the current file name and line number with the log output
 func Debug(format string, a ...interface{}) {
-	_, file, line, ok := runtime.Caller(1)
-	if !ok {
-		log.Fatal("Debug() failed")
-	}
-
-	_, fileName := path.Split(file)
-
-	args := []interface{}{fileName, line}
-	args = append(args, a...)
-	log.Debugf("%s:%d \n"+format, args...)
+	// Removed in branch feat/job_templates, to be filled in separate branch
 }
 
 // Prints line and file and then exits with fatal error message
 func Fatalf(format string, a ...interface{}) {
-	_, file, line, ok := runtime.Caller(1)
-	if !ok {
-		log.Fatal("Fatal() failed")
-	}
-
-	_, fileName := path.Split(file)
-
-	args := []interface{}{fileName, line}
-	args = append(args, a...)
-	log.Fatalf("%s:%d \n"+format, args...)
+	// Removed in branch feat/job_templates, to be filled in separate branch
 }
 
 // Wrapper for single value output
